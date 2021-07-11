a1f8ec5880f7560fb1d847e42cad0c03aaf4714b
witway@korea.ac.kr <witway@korea.ac.kr>
2021-07-11 19:14:46 +0900

    django course 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accountapp.views import hello_world
-app_name = "accountapp"
+from accountapp.views import hello_world, AccountCreateView
+app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world')
+    path('hello_world/', hello_world, name = 'hello_world'),
+    path('create/', AccountCreateView.as_view(), name = 'create'),
 ]
