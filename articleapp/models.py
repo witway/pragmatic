88390e556ae1937eecefd47b74a0d739498e868a
witway@korea.ac.kr <witway@korea.ac.kr>
2021-07-14 15:19:09 +0900

    django course 35 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="article", null=True)
+    # User가 탈퇴했을 때, 게시글은 알 수 없음과 같은 창이 뜨게끔
+    # User가 Article을 접근할 때 사용하는 이름이므로 article이라고 해줌
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
