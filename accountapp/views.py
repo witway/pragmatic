bfd568ba82b0cde7aecb48eb63d6d9c85556d3f9
witway <84003805+witway@users.noreply.github.com>
2021-07-18 17:56:33 +0900

    django course 41 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,10 +8,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required()]
 
@@ -57,10 +59,16 @@ class AccountDeleteView(DeleteView):
     #     else:
     #         return HttpResponseForbidden()
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 @method_decorator(has_ownership, 'get') #권한이 있는 사용자가 아니면, forbidden 페이지를 출력해줌
 @method_decorator(has_ownership, 'post')
--- articleapp/models.py
+++ articleapp/models.py
@@ -2,11 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="article", null=True)
     # User가 탈퇴했을 때, 게시글은 알 수 없음과 같은 창이 뜨게끔
     # User가 Article을 접근할 때 사용하는 이름이므로 article이라고 해줌
 
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name="article", null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
